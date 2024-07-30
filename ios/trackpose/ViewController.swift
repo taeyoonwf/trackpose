93f0b6fbeff8b45750f908835957db368602e385
taeyoonwf <taeyoonwf@gmail.com>
2024-07-31 01:15:03 +0900

    implement live streaming

--- ios/trackpose/ViewController.swift
+++ ios/trackpose/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
   var subscriber: UnsafeMutableRawPointer?
   let queue = DispatchQueue(label: "com.yourapp.zeromq", qos: .background)
 
+  @IBOutlet private var imageView: UIImageView!
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
@@ -39,7 +41,7 @@ class ViewController: UIViewController {
       guard let self = self else { return }
 
       var pollItems = [zmq_pollitem_t(socket: self.subscriber, fd: 0, events: Int16(ZMQ_POLLIN), revents: 0)]
-      let bufsize = 32768
+      let bufsize = 65536
       var buffer = [UInt8](repeating: 0, count: bufsize)
 
       while true {
@@ -53,7 +55,7 @@ class ViewController: UIViewController {
         if pollItems[0].revents & Int16(ZMQ_POLLIN) != 0 {
           let size = zmq_recv(self.subscriber, &buffer, bufsize, 0)
           if size > 0 {
-            let message = String(bytes: buffer, encoding: .utf8) ?? ""
+            let message = String(bytes: buffer[..<Int(size)], encoding: .utf8) ?? ""
             DispatchQueue.main.async {
                 self.handleMessage(message)
             }
@@ -65,7 +67,30 @@ class ViewController: UIViewController {
 
   func handleMessage(_ message: String) {
       // Handle the received message
-      print("Received message: \(message)")
+      //print("Received message: \(message)")
+    if let jsonData = message.data(using: .utf8) {
+      do {
+        if let json = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any] {
+          let frame = json["frame"] as? String
+          let timestamp = json["timestamp"] as? Int64
+          if frame != nil {
+            if let imageData = Data(base64Encoded: frame!) {
+              if let image = UIImage(data: imageData) {
+                self.imageView.image = image
+                self.imageView.contentMode = .scaleAspectFit
+                //self.imageView.layer.transform = CATransform3DMakeAffineTransform(CGAffineTransformMakeRotation(M_PI_2))
+                //self.imageView.layer.transform = CATransform3DMakeRotation(90, <#T##x: CGFloat##CGFloat#>, <#T##y: CGFloat##CGFloat#>, <#T##z: CGFloat##CGFloat#>) CGAffineTransformMakeRotation(90)
+                self.imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+              }
+            }
+          }
+        }
+      } catch {
+        print("failed to parse JSON: \(error.localizedDescription)")
+      }
+    }
+    //var data: NSData = message.dataUsingEncoding(NSUTF8StringEncoding)!
+    print("asdf")
   }
 
   deinit {
