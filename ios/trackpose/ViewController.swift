26e2dffe4c2ec131788373d2d0deb1b938d51311
taeyoonwf <taeyoonwf@gmail.com>
2024-07-30 23:25:00 +0900

    use polling

--- ios/trackpose/ViewController.swift
+++ ios/trackpose/ViewController.swift
@@ -35,20 +35,32 @@ class ViewController: UIViewController {
   }
 
   func startListening() {
-      queue.async { [weak self] in
-          guard let self = self else { return }
+    queue.async { [weak self] in
+      guard let self = self else { return }
 
-          while true {
-              var buffer = [UInt8](repeating: 0, count: 25600)
-              let size = zmq_recv(self.subscriber, &buffer, 25600, 0)
-              if size > 0 {
-                  let message = String(bytes: buffer, encoding: .utf8) ?? ""
-                  DispatchQueue.main.async {
-                      self.handleMessage(message)
-                  }
-              }
+      var pollItems = [zmq_pollitem_t(socket: self.subscriber, fd: 0, events: Int16(ZMQ_POLLIN), revents: 0)]
+      let bufsize = 32768
+      var buffer = [UInt8](repeating: 0, count: bufsize)
+
+      while true {
+        let rc = zmq_poll(&pollItems, 1, 8)
+            
+        if rc == -1 {
+          print("polling error")
+          break
+        }
+            
+        if pollItems[0].revents & Int16(ZMQ_POLLIN) != 0 {
+          let size = zmq_recv(self.subscriber, &buffer, bufsize, 0)
+          if size > 0 {
+            let message = String(bytes: buffer, encoding: .utf8) ?? ""
+            DispatchQueue.main.async {
+                self.handleMessage(message)
+            }
           }
+        }
       }
+    }
   }
 
   func handleMessage(_ message: String) {
