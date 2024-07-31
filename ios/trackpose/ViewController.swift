ecab96b0174a0990a9125229e287ab404286afe9
taeyoonwf <taeyoonwf@gmail.com>
2024-07-31 10:24:46 +0900

    update UI

--- ios/trackpose/ViewController.swift
+++ ios/trackpose/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
   var context: UnsafeMutableRawPointer?
   var subscriber: UnsafeMutableRawPointer?
   let queue = DispatchQueue(label: "com.yourapp.zeromq", qos: .background)
+  var resized: Bool = false
 
   @IBOutlet private var imageView: UIImageView!
 
@@ -76,11 +77,24 @@ class ViewController: UIViewController {
           if frame != nil {
             if let imageData = Data(base64Encoded: frame!) {
               if let image = UIImage(data: imageData) {
-                self.imageView.image = image
-                self.imageView.contentMode = .scaleAspectFit
-                //self.imageView.layer.transform = CATransform3DMakeAffineTransform(CGAffineTransformMakeRotation(M_PI_2))
+                let lsimage = UIImage(cgImage: image.cgImage!, scale: 1, orientation: UIImage.Orientation.right)
+                self.imageView.image = lsimage
+                //self.imageView.contentMode = .scaleAspectFit //.scaleToFill //.scaleAspectFill//.scaleAspectFit
+                print("imageViewFrame : \(self.imageView.frame.width) x \(self.imageView.frame.height)")
+                //print("imageCgSize : \(self.imageView.image?.cgImage) x \(self.imageView.image?.cgImage)")
+                print("image : \(lsimage.size.width) x \(lsimage.size.height)")
+                print("imageViewBounds : \(self.imageView.bounds.size)")
+                print("mins \(self.imageView.frame.minX) \(self.imageView.frame.minY) \(self.imageView.frame.maxX) \(self.imageView.frame.maxY) ")
+                
+                
+                if resized == false {
+                  var minRatio = min(lsimage.size.width / self.imageView.frame.width, lsimage.size.height / self.imageView.frame.height)
+                  //minRatio = 1
+                  //self.imageView.layer.transform = CATransform3DMakeAffineTransform(CGAffineTransformMakeRotation(Double.pi / 2).concatenating(CGAffineTransformMakeScale(1/minRatio, 1/minRatio)))
+                  resized = true
+                }
                 //self.imageView.layer.transform = CATransform3DMakeRotation(90, <#T##x: CGFloat##CGFloat#>, <#T##y: CGFloat##CGFloat#>, <#T##z: CGFloat##CGFloat#>) CGAffineTransformMakeRotation(90)
-                self.imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+                //self.imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
               }
             }
           }
@@ -90,7 +104,7 @@ class ViewController: UIViewController {
       }
     }
     //var data: NSData = message.dataUsingEncoding(NSUTF8StringEncoding)!
-    print("asdf")
+    //print("asdf")
   }
 
   deinit {
@@ -98,5 +112,6 @@ class ViewController: UIViewController {
       zmq_close(subscriber)
       zmq_ctx_destroy(context)
   }
-}
+  
 
+}
