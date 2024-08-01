5fc3f628a97f2166b2982fd229da43ed758d6193
taeyoonwf <taeyoonwf@gmail.com>
2024-08-01 20:03:36 +0900

    layout bugfix

--- ios/trackpose/ViewController.swift
+++ ios/trackpose/ViewController.swift
@@ -12,12 +12,22 @@ class ViewController: UIViewController {
   var context: UnsafeMutableRawPointer?
   var subscriber: UnsafeMutableRawPointer?
   let queue = DispatchQueue(label: "com.yourapp.zeromq", qos: .background)
-  var resized: Bool = false
+  //var resized: Bool = false
+  var lastFrame: UIImage?
+  var lastPubTime: Int64?
+  var lastSubTime: Int64?
+  var pubSubDiff: Int64?
 
   @IBOutlet private var imageView: UIImageView!
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    UIApplication.shared.isIdleTimerDisabled = true
+    if UIDevice.current.userInterfaceIdiom == .pad {
+      //imageView.frame.size = CGSize(width: imageView.frame.width, height: imageView.frame.height - 109)
+    } else {
+      //imageView.frame.size = CGSize(width: imageView.frame.width, height: imageView.frame.height - 59)
+    }
     // Do any additional setup after loading the view.
 
     context = zmq_ctx_new()
@@ -42,7 +52,7 @@ class ViewController: UIViewController {
       guard let self = self else { return }
 
       var pollItems = [zmq_pollitem_t(socket: self.subscriber, fd: 0, events: Int16(ZMQ_POLLIN), revents: 0)]
-      let bufsize = 65536
+      var bufsize = 1024
       var buffer = [UInt8](repeating: 0, count: bufsize)
 
       while true {
@@ -55,7 +65,11 @@ class ViewController: UIViewController {
             
         if pollItems[0].revents & Int16(ZMQ_POLLIN) != 0 {
           let size = zmq_recv(self.subscriber, &buffer, bufsize, 0)
-          if size > 0 {
+          if size >= bufsize {
+            bufsize *= 2
+            buffer = [UInt8](repeating: 0, count: bufsize)
+          }
+          else if size > 0 {
             let message = String(bytes: buffer[..<Int(size)], encoding: .utf8) ?? ""
             DispatchQueue.main.async {
                 self.handleMessage(message)
@@ -80,19 +94,19 @@ class ViewController: UIViewController {
                 let lsimage = UIImage(cgImage: image.cgImage!, scale: 1, orientation: UIImage.Orientation.right)
                 self.imageView.image = lsimage
                 //self.imageView.contentMode = .scaleAspectFit //.scaleToFill //.scaleAspectFill//.scaleAspectFit
-                print("imageViewFrame : \(self.imageView.frame.width) x \(self.imageView.frame.height)")
+                //print("imageViewFrame : \(self.imageView.frame.width) x \(self.imageView.frame.height)")
                 //print("imageCgSize : \(self.imageView.image?.cgImage) x \(self.imageView.image?.cgImage)")
-                print("image : \(lsimage.size.width) x \(lsimage.size.height)")
-                print("imageViewBounds : \(self.imageView.bounds.size)")
-                print("mins \(self.imageView.frame.minX) \(self.imageView.frame.minY) \(self.imageView.frame.maxX) \(self.imageView.frame.maxY) ")
+                //print("image : \(lsimage.size.width) x \(lsimage.size.height)")
+                //print("imageViewBounds : \(self.imageView.bounds.size)")
+                //print("mins \(self.imageView.frame.minX) \(self.imageView.frame.minY) \(self.imageView.frame.maxX) \(self.imageView.frame.maxY) ")
                 
                 
-                if resized == false {
-                  var minRatio = min(lsimage.size.width / self.imageView.frame.width, lsimage.size.height / self.imageView.frame.height)
+                //if resized == false {
+                  //var minRatio = min(lsimage.size.width / self.imageView.frame.width, lsimage.size.height / self.imageView.frame.height)
                   //minRatio = 1
                   //self.imageView.layer.transform = CATransform3DMakeAffineTransform(CGAffineTransformMakeRotation(Double.pi / 2).concatenating(CGAffineTransformMakeScale(1/minRatio, 1/minRatio)))
-                  resized = true
-                }
+                  //resized = true
+                //}
                 //self.imageView.layer.transform = CATransform3DMakeRotation(90, <#T##x: CGFloat##CGFloat#>, <#T##y: CGFloat##CGFloat#>, <#T##z: CGFloat##CGFloat#>) CGAffineTransformMakeRotation(90)
                 //self.imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
               }
